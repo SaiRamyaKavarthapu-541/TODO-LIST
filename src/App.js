713e44ccf8c72532842a6b489eebc79473859028
SaiRamyaKavarthapu-541 <83948057+SaiRamyaKavarthapu-541@users.noreply.github.com>
2021-05-28 09:50:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+import Form from './component/Form';
+import MockData from './mock/AppMock';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 ><center> {this.props.Content.heading}</center></h1>
+        <div>
+          <div>
+            <Form />
+          </div>
+        </div>
+
+      </div>
+
+    );
+  }
+}
+App.defaultProps = { Content: MockData }
+export default App;
