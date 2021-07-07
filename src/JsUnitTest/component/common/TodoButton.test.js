08b581b14583ee5987684843449b0763347c05bd
SaiRamyaKavarthapu-541 <83948057+SaiRamyaKavarthapu-541@users.noreply.github.com>
2021-07-07 21:49:35 +0530

    Add files via upload

---
+++ src/JsUnitTest/component/common/TodoButton.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import sinon from 'sinon';
+import TodoButton from '../../../component/common/TodoButton';
+
+
+describe('Todo Button', () => {
+  it('Test click event', () => {
+    const todoButtonClick = sinon.spy();
+    const button = shallow((<TodoButton onClick={todoButtonClick}/>));
+    button.find('button').simulate('click');
+    expect(todoButtonClick.calledOnce).toBe(true)
+  });
+});
