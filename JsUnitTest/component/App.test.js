be3a231e67a3b0781a2e0b7ea8d25fab4fd7ee90
SaiRamyaKavarthapu-541 <83948057+SaiRamyaKavarthapu-541@users.noreply.github.com>
2021-07-07 21:42:24 +0530

    Delete App.test.js

--- JsUnitTest/component/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from '../../App';
-
-describe('App component', () => {
-    test("renders App component" , ()=>{
-        const wrapper = shallow(<App />); 
-    expect(wrapper.exists()).toBe(true);
-  })});
